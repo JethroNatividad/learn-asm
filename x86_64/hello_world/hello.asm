a52274fb70fbc723e598fe6a041f110679cbb5a1
JethroNatividad <natividad.jet@gmail.com>
2024-02-11 10:49:51 +0800

    update hello

--- x86_64/hello_world/hello.asm
+++ x86_64/hello_world/hello.asm
@@ -1,16 +1,33 @@
 section .data
-    text db "Hello, World!",10
+    prompt db "What's your name? "
+    prompt_len equ $ - prompt
  
+section .bss
+    input_len equ 100
+    input resb input_len
+
 section .text
     global _start
  
 _start:
     mov rax, 1
     mov rdi, 1
-    mov rsi, text
-    mov rdx, 14
+    mov rsi, prompt
+    mov rdx, prompt_len
     syscall
- 
-    mov rax, 60
+
+    mov rax, 0
     mov rdi, 0
+    mov rsi, input
+    mov rdx, input_len
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, input
+    mov rdx, input_len
+    syscall
+
+    mov rax, 60
+    mov rdi, 1
     syscall
