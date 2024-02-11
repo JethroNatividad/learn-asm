9ef81d9bd11766bcb6d65ef5ec200fcbe80e5e97
JethroNatividad <natividad.jet@gmail.com>
2024-02-11 12:47:55 +0800

    show prompt

--- x86_64/character_count/main.asm
+++ x86_64/character_count/main.asm
@@ -2,3 +2,20 @@
 ; inputs: input
 ; process: count the number of characters
 ; output: the number of characters
+
+section .data
+    prompt db "What is the input string? "
+    prompt_len equ $ - prompt
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt
+    mov rdx, prompt_len
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
