5b17f33f2d4e5a73fe8506938e88515fbddbb9cb
JethroNatividad <natividad.jet@gmail.com>
2024-02-11 12:49:26 +0800

    get input

--- x86_64/character_count/main.asm
+++ x86_64/character_count/main.asm
@@ -6,6 +6,11 @@
 section .data
     prompt db "What is the input string? "
     prompt_len equ $ - prompt
+
+section .bss
+    input_len equ 100
+    input resb input_len
+
 section .text
     global _start
 
@@ -14,6 +19,12 @@ _start:
     mov rdi, 1
     mov rsi, prompt
     mov rdx, prompt_len
+    syscall
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi input
+    mov rdx input_len
     syscall
 
     mov rax, 60
