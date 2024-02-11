976e0d31f595d3357d4859bcdad8baf000a2a3d1
JethroNatividad <natividad.jet@gmail.com>
2024-02-11 09:18:12 +0800

    one line command

--- x86_64/say_hello/main.asm
+++ x86_64/say_hello/main.asm
@@ -2,3 +2,20 @@
 ; inputs: name
 ; process: concat the name to "Hello, {name}, nice to meet you!"
 ; output: the message
+
+section .data
+    text db "Hello, World!", 10 ;define bytes "Hello, World!\n"
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text
+    mov rdx, 14
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
