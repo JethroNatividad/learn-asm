88fc7a55ad2b24eeb682354ae52a0da990aaae7a
JethroNatividad <natividad.jet@gmail.com>
2024-02-13 22:40:36 +0800

    get and show input

--- 8086/say_hello/main.asm
+++ 8086/say_hello/main.asm
@@ -2,15 +2,35 @@
 .stack
 
 .data
-message db "Hello World!$"
+message db "What is your name? $"
+
+max_input_size db 100
+actual_input_size db ?
+input db 100 dup(0) ; buffer
 
 .code
 start:
     MOV AX, @data
     MOV DS, AX
 
+    MOV AH, 00H
+    INT 10H
+
     MOV AH, 09H
     MOV DX, offset message
+    INT 21H
+
+    LEA DX, max_input_size
+    MOV AH, 0AH
+    INT 21H
+
+    MOV AL, "$"
+    MOV SI, offset input
+    ADD SI, offset actual_input_size
+    MOV [SI], AL
+
+    MOV AH, 09H
+    MOV DX, offset input
     INT 21H
 
     MOV AH, 4CH
