936072c1f2d687c84966ba1c72a721e5240c47cd
JethroNatividad <natividad.jet@gmail.com>
2024-02-15 10:12:38 +0800

    write calls

--- 8086/madlib/main.asm
+++ 8086/madlib/main.asm
@@ -10,6 +10,21 @@ start:
     MOV AX, @data
     MOV DS, AX
 
+    CALL show_prompt_noun
+    CALL get_noun
+
+    CALL show_prompt_verb
+    CALL get_verb
+
+    CALL show_prompt_adverb
+    CALL get_adverb
+
+    CALL show_prompt_adjective
+    CALL get_adjective
+
+    CALL show_output
+
+
     MOV AH, 4CH
     INT 21H
 end
