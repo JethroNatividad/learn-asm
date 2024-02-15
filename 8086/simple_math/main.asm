cf4708094dd67c71b47242e32c75fd11148e3ad0
JethroNatividad <natividad.jet@gmail.com>
2024-02-16 06:58:22 +0800

    expand problem

--- 8086/simple_math/main.asm
+++ 8086/simple_math/main.asm
@@ -1,4 +1,13 @@
 ; Write a program that takes two numbers, and outputs the addition, subtraction, multiplication, and division.
+; Inputs: first_number, second_number
+; Process: convert to decimal, calculate addition, subtraction, multiplication, division, convert to ASCII.
+; Outputs: 
+; What is the first number? 10
+; What is the second number? 5
+; 10 + 5 = 15
+; 10 - 5 = 5
+; 10 * 5 = 50
+; 10 / 5 = 2
 
 .model small
 
