a84a803af8c53c7670f3c4dccf8696879c7ca406
JethroNatividad <natividad.jet@gmail.com>
2024-02-18 20:05:53 +0800

    subtract remainig

--- 8086/retirement_calculator/main.asm
+++ 8086/retirement_calculator/main.asm
@@ -3,11 +3,17 @@
 .stack
 
 .data
+age db 25
+retire_age db 65
 
 .code
 start:
     MOV AX, @data
     MOV DS, AX
+
+    MOV AL, age
+    MOV BL, retire_age
+    SUB BL, AL
 
     MOV AH, 4CH
     INT 21H
