3aa763f3793476cd8987163f435b34448d22f9c8
JethroNatividad <natividad.jet@gmail.com>
2024-02-18 20:23:59 +0800

    get years until retire

--- 8086/retirement_calculator/main.asm
+++ 8086/retirement_calculator/main.asm
@@ -9,15 +9,18 @@
 .data
 age db 25
 retire_age db 65
+years_until_retire dw ?
 
 .code
 start:
     MOV AX, @data
     MOV DS, AX
 
+    XOR AX, AX
     MOV AL, age
     MOV BL, retire_age
-    SUB BL, AL
+    SUB BX, AX
+    MOV years_until_retire, BX
 
     MOV AH, 4CH
     INT 21H
