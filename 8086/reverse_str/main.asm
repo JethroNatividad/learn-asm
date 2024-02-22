940ac482621636ac6d0cd2862beb71fcc41cd6df
JethroNatividad <natividad.jet@gmail.com>
2024-02-22 19:45:04 +0800

    implement get input

--- 8086/reverse_str/main.asm
+++ 8086/reverse_str/main.asm
@@ -12,8 +12,24 @@ start:
     MOV AX, @data
     MOV DS, AX
 
-
+    MOV DX, max_input_size
+    CALL get_input
 
     MOV AH, 4CH
     INT 21H
+
+; inputs: max input size in DX
+get_input:
+    MOV AH, 0AH
+    INT 21H
+
+    ; add $ to end of str
+    LEA SI, DX
+    INC SI
+    MOV BX, [SI] ; input size
+    INC SI
+    ADD SI, BX
+    MOV [SI], "$"
+    RET
+    
 end
