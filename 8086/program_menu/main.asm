0be9e5e73989ca7cfa636f857cd215f1d940f771
JethroNatividad <natividad.jet@gmail.com>
2024-05-03 01:25:29 +0800

    change act 2

--- 8086/program_menu/main.asm
+++ 8086/program_menu/main.asm
@@ -33,11 +33,6 @@ Surname_8 DB 'Tulawe', '$'
 ; End Activity 1 Variables
 
 ; Activity 2 Variables
-press_any_key_msg DB 10, 13, 'Press any key to continue', '$'
-clear_string DB '                          ', '$'
-; End Activity 2 Variables
-
-; Activity 3 Variables
 name_prompt db 'What is your name? ', '$'
 pre_name db 'Name: ', '$'
 
@@ -51,7 +46,7 @@ name_field db name_max_length dup(' ') ; buffer
 age_max_length db 100
 age_actual_length db ?
 age_field db age_max_length dup(' ') ; buffer
-; End Activity 3 Variables
+; End Activity 2 Variables
 
 .code
 start:
@@ -252,190 +247,62 @@ start:
             MOV AH, 09H
             INT 21H
             JMP exit
+        
         activity_2:
             CALL clear_screen
-            ; Surname 1
-            ; Set cursor position
-            MOV DH, 0 ; Row number
-            MOV DL, 0 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt 
-            
-            ; Load address of the string
-            LEA DX, Surname_1
-            
-            ; Output the string loaded in DX
+            ; --- Prompt for name --- 
+            ; Print prompt
             MOV AH, 09H
+            LEA DX, name_prompt
             INT 21H
 
-            CALL PROMPT_NEXT
-                
-            ; Surname 2
-            ; Set cursor position
-            MOV DH, 2 ; Row number
-            MOV DL, 7 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt 
-
-            
-            ; Load address of the string
-            LEA DX, Surname_2
-            
-            ; Output the string loaded in DX
-            MOV AH, 09H
-            INT 21H
-            
-            CALL PROMPT_NEXT
-
-            ; Surname 3
-            ; Set cursor position
-            MOV DH, 4 ; Row number
-            MOV DL, 12 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt 
-            
-            ; Load address of the string
-            LEA DX, Surname_3
-            
-            ; Output the string loaded in DX
-            MOV AH, 09H
+            ; Get Input
+            MOV AH, 0AH
+            LEA DX, name_max_length
             INT 21H
 
-            CALL PROMPT_NEXT
-            
-            ; Surname 4
-            ; Set cursor position
-            MOV DH, 6 ; Row number
-            MOV DL, 20 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt
-            
-            ; Load address of the string
-            LEA DX, Surname_4
-            
-            ; Output the string loaded in DX
+            ; Add $ to end of input
+            XOR BH, BH
+            MOV BL, name_actual_length
+            MOV name_field[BX], '$'
+
+            ; --- Prompt for age ---
+            ; Print prompt
             MOV AH, 09H
+            LEA DX, age_prompt
             INT 21H
 
-            CALL PROMPT_NEXT
-            
-            ; Surname 5
-            ; Set cursor position
-            MOV DH, 8 ; Row number
-            MOV DL, 25 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt
-
-            
-            ; Load address of the string
-            LEA DX, Surname_5
-            
-            ; Output the string loaded in DX
-            MOV AH, 09H
+            ; Get Input
+            MOV AH, 0AH
+            LEA DX, age_max_length
             INT 21H
 
-            CALL PROMPT_NEXT
-            
-            ; Surname 6
-            ; Set cursor position
-            MOV DH, 10 ; Row number
-            MOV DL, 34 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt
-            
-            ; Load address of the string
-            LEA DX, Surname_6
-            
-            ; Output the string loaded in DX
+            ; Add $ to end of input
+            XOR BH, BH
+            MOV BL, age_actual_length
+            MOV age_field[BX], '$'
+
+            ; Clear screen
+            MOV AH, 00H
+            MOV AL, 03H
+            INT 10H
+
+
+            ; Print name
             MOV AH, 09H
+            LEA DX, pre_name
+            INT 21H
+            LEA DX, name_field
+            INT 21H
+            
+            ; Print age
+            MOV AH, 09H
+            LEA DX, pre_age
+            INT 21H
+            LEA DX, age_field
             INT 21H
 
-            CALL PROMPT_NEXT
-            
-            ; Surname 7
-            ; Set cursor position
-            MOV DH, 12 ; Row number
-            MOV DL, 43 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt
-
-            ; Load address of the string
-            LEA DX, Surname_7
-            
-            ; Output the string loaded in DX
-            MOV AH, 09H
-            INT 21H 
-
-            CALL PROMPT_NEXT
-            
-            ; Surname 8
-            ; Set cursor position
-            MOV DH, 14 ; Row number
-            MOV DL, 50 ; Column number
-            MOV AH, 02H ; Function to set cursor position
-            INT 10H     ; BIOS interrupt
-
-            
-            ; Load address of the string
-            LEA DX, Surname_8
-            
-            ; Output the string loaded in DX
-            MOV AH, 09H
-            INT 21H
-
-        activity_3:
-        CALL clear_screen
-        ; --- Prompt for name --- 
-        ; Print prompt
-        MOV AH, 09H
-        LEA DX, name_prompt
-        INT 21H
-
-        ; Get Input
-        MOV AH, 0AH
-        LEA DX, name_max_length
-        INT 21H
-
-        ; Add $ to end of input
-        XOR BH, BH
-        MOV BL, name_actual_length
-        MOV name_field[BX], '$'
-
-        ; --- Prompt for age ---
-        ; Print prompt
-        MOV AH, 09H
-        LEA DX, age_prompt
-        INT 21H
-
-        ; Get Input
-        MOV AH, 0AH
-        LEA DX, age_max_length
-        INT 21H
-
-        ; Add $ to end of input
-        XOR BH, BH
-        MOV BL, age_actual_length
-        MOV age_field[BX], '$'
-
-        ; Clear screen
-        MOV AH, 00H
-        MOV AL, 03H
-        INT 10H
-
-
-        ; Print name
-        MOV AH, 09H
-        LEA DX, pre_name
-        INT 21H
-        LEA DX, name_field
-        INT 21H
-        
-        ; Print age
-        MOV AH, 09H
-        LEA DX, pre_age
-        INT 21H
-        LEA DX, age_field
-        INT 21H
+            JMP exit
         exit:
 
         MOV AH, 4CH
@@ -452,24 +319,4 @@ clear_screen:
     int 10h
     RET
 
-; For activity 2
-PROMPT_NEXT:
-    LEA DX, press_any_key_msg ; print msg
-    MOV AH, 09H
-    INT 21H
-
-    MOV AH, 01H ; wait for input
-    INT 21H
-
-    MOV AH, 03H  ; Get cursor position
-    INT 10H
-
-    MOV AH, 02H  ; Set cursor position
-    INT 10H
-
-    MOV AH, 09H  ; Display string
-    LEA DX, clear_string
-    INT 21H
-
-    RET
 end start
