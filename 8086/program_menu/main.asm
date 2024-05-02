f8a0c4eb66993edd90c36db326423f2285ee3d71
JethroNatividad <natividad.jet@gmail.com>
2024-05-03 02:15:48 +0800

    Add activity 4

--- 8086/program_menu/main.asm
+++ 8086/program_menu/main.asm
@@ -76,6 +76,36 @@ largest_number_label db 'The largest number is: ', '$'
 largest_number_str db 100 dup(' ')
 ; End Activity 3 Variables
 
+; Activity 4 Variables
+verb_prompt db "Enter a verb: $"
+noun_prompt db 10, 13, "Enter a noun: $"
+occupation_prompt db 10, 13, "Enter an occupation: $"
+adjective_prompt db 10, 13, "Enter an adjective: $"
+
+max_input_noun_size db 100
+input_noun_size db ?
+input_noun_buffer db max_input_noun_size dup(' ')
+
+max_input_verb_size db 100
+input_verb_size db ?
+input_verb_buffer db max_input_verb_size dup(' ')
+
+max_input_occupation_size db 100
+input_occupation_size db ?
+input_occupation_buffer db max_input_occupation_size dup(' ')
+
+max_input_adjective_size db 100
+input_adjective_size db ?
+input_adjective_buffer db max_input_adjective_size dup(' ')
+
+output1 db 10, 13, "One day our teacher decided to quit his job and apply to be the school's $"
+output2 db ". The employer of the university however thought that he was too $"
+output3 db ".  So our teacher decided to $"
+output4 db ", not knowing that his $"
+output5 db " broke down, costing him the job $"
+
+; End Activity 4 Variables
+
 .code
 start:
     MOV AX, @data
@@ -143,7 +173,7 @@ start:
         CMP choice_field, '3'
         JE activity_3
         CMP choice_field, '4'
-        JE activity_1
+        JE activity_4
         CMP choice_field, '5'
         JE activity_1
         CMP choice_field, '6'
@@ -417,7 +447,7 @@ start:
             MOV AH, 09H
             LEA DX, newline
             INT 21H
-            
+
             MOV AH, 09H
             LEA DX, largest_number_label
             INT 21H
@@ -427,6 +457,58 @@ start:
             INT 21H
 
             JMP exit
+
+        activity_4:
+            CALL clear_screen
+
+            MOV DX, offset verb_prompt
+            CALL print
+            MOV DX, offset max_input_verb_size
+            CALL get_input
+
+            MOV DX, offset noun_prompt
+            CALL print
+            MOV DX, offset max_input_noun_size
+            CALL get_input
+
+            MOV DX, offset occupation_prompt
+            CALL print
+            MOV DX, offset max_input_occupation_size
+            CALL get_input
+
+            MOV DX, offset adjective_prompt
+            CALL print
+            MOV DX, offset max_input_adjective_size
+            CALL get_input
+
+            ; Print output
+            LEA DX, output1
+            CALL print
+
+            LEA DX, input_occupation_buffer
+            CALL print
+
+            LEA DX, output2
+            CALL print
+
+            LEA DX, input_adjective_buffer
+            CALL print
+
+            LEA DX, output3
+            CALL print
+
+            LEA DX, input_verb_buffer
+            CALL print
+
+            LEA DX, output4
+            CALL print
+
+            LEA DX, input_noun_buffer
+            CALL print
+
+            LEA DX, output5
+            CALL print
+
         exit:
 
         MOV AH, 4CH
@@ -485,5 +567,19 @@ num_to_str_loop_2:
     MOV [SI], "$"
     RET
 
+; input: max input size in DX
+get_input:
+    MOV AH, 0AH
+    INT 21H
+
+    ; ADD $ to end
+    LEA SI, DX ; max input size address
+    INC SI 
+    XOR BX, BX
+    MOV BL, [SI] ; input size
+    INC SI
+    ADD SI, BX ; buffer location
+    MOV [SI], "$"
+    RET
 
 end start
