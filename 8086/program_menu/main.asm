105c2e18cdd93d669ba36aaa9319d4eeea0c80c3
JethroNatividad <natividad.jet@gmail.com>
2024-05-03 01:22:42 +0800

    add activity 3

--- 8086/program_menu/main.asm
+++ 8086/program_menu/main.asm
@@ -37,6 +37,22 @@ press_any_key_msg DB 10, 13, 'Press any key to continue', '$'
 clear_string DB '                          ', '$'
 ; End Activity 2 Variables
 
+; Activity 3 Variables
+name_prompt db 'What is your name? ', '$'
+pre_name db 'Name: ', '$'
+
+age_prompt db 10, 13, 'How old are you? ', '$'
+pre_age db 10, 13, 'Age: ', '$'
+
+name_max_length db 100
+name_actual_length db ?
+name_field db name_max_length dup(' ') ; buffer
+
+age_max_length db 100
+age_actual_length db ?
+age_field db age_max_length dup(' ') ; buffer
+; End Activity 3 Variables
+
 .code
 start:
     MOV AX, @data
@@ -366,6 +382,60 @@ start:
             ; Output the string loaded in DX
             MOV AH, 09H
             INT 21H
+
+        activity_3:
+        CALL clear_screen
+        ; --- Prompt for name --- 
+        ; Print prompt
+        MOV AH, 09H
+        LEA DX, name_prompt
+        INT 21H
+
+        ; Get Input
+        MOV AH, 0AH
+        LEA DX, name_max_length
+        INT 21H
+
+        ; Add $ to end of input
+        XOR BH, BH
+        MOV BL, name_actual_length
+        MOV name_field[BX], '$'
+
+        ; --- Prompt for age ---
+        ; Print prompt
+        MOV AH, 09H
+        LEA DX, age_prompt
+        INT 21H
+
+        ; Get Input
+        MOV AH, 0AH
+        LEA DX, age_max_length
+        INT 21H
+
+        ; Add $ to end of input
+        XOR BH, BH
+        MOV BL, age_actual_length
+        MOV age_field[BX], '$'
+
+        ; Clear screen
+        MOV AH, 00H
+        MOV AL, 03H
+        INT 10H
+
+
+        ; Print name
+        MOV AH, 09H
+        LEA DX, pre_name
+        INT 21H
+        LEA DX, name_field
+        INT 21H
+        
+        ; Print age
+        MOV AH, 09H
+        LEA DX, pre_age
+        INT 21H
+        LEA DX, age_field
+        INT 21H
         exit:
 
         MOV AH, 4CH
